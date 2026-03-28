f88d7115bed96848aada6d8d23955167505421dc
Julius <julius.rupsys@bentley.com>
2026-03-28 03:03:28 +0200

    autocompletion

--- init.lua
+++ init.lua
@@ -805,6 +805,9 @@ require('lazy').setup({
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
+        accept = {
+          auto_brackets = { enabled = false },
+        },
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
       },
 
